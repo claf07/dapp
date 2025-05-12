16922fb61bf6abdabc7bb3b4388c4354b07c4cd8
Claflin C R <42567965-claflincollins@users.noreply.replit.com>
2025-05-12 12:19:59 +0000

    Restored to '930fc8f23bb18ebe84cd6f1af357b37beb41d91d'
    Replit-Restored-To: 930fc8f23bb18ebe84cd6f1af357b37beb41d91d

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
+    ganache: {
+      url: "http://0.0.0.0:7545",
       chainId: 1337
     }
   }
